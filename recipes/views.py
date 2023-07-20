68544dbce384b97076e514723dda648ac6ff8067
Eleandro Marins <eleandromartins23@hotmail.com>
2023-07-20 17:53:58 -0300

    imagem carregada

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html')
-
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
