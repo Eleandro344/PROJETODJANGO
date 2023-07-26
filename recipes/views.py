00e66224eaf7204735e474215217a4f5c1723e3a
Eleandro Marins <eleandromartins23@hotmail.com>
2023-07-25 23:26:07 -0300

    nova views

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
+
+
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Eleandro Martins, '
     })
+
+
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
-    })
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Eleandro Martins,'
+
+        })
